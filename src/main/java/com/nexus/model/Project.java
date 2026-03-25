8410da72711be34e1f5287565abfad813804443a
Bernardo Sinoti Sabbag <bernardosinotis@gmail.com>
2026-03-24 22:00:12 -0300

    projectatualizado

--- src/main/java/com/nexus/model/Project.java
+++ src/main/java/com/nexus/model/Project.java
@@ -1,20 +1,47 @@
 package com.nexus.model;
 
-import com.nexus.exception.NexusValidationException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.nexus.exception.NexusValidationException;
 
 public class Project {
     private final String name;
     private final List<Task> tasks;
     private final int totalBudget;
 
-    public Project(String name, List<Task> tasks, int totalBudget) {
+    public Project(String name, int totalBudget) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("O nome do projeto não pode ser vazio.");
         }
         if (totalBudget <= 0) {
             throw new IllegalArgumentException("O orçamento não pode ser igual ou inferior a zero.");
         }
+        this.name = name;
+        this.tasks = new ArrayList<>();
+        this.totalBudget = totalBudget;
     }
+
+    public void addTask(Task t) {
+        if (t == null) {
+            throw new IllegalArgumentException("A tarefa não pode ser nula.");
+        }
+
+        int sumHours = tasks.stream()
+                        .mapToInt(Task::getEstimatedEffort)
+                        .sum();
+
+        if (sumHours + t.getEstimatedEffort() > totalBudget) {
+            Task.totalValidationErrors++;
+            throw new NexusValidationException("O tempo excede o disponível.");
+        }
+
+        this.tasks.add(t);
+
+    }
+
+    public String getName() { return name; }
+    public int getTotalBudget() { return totalBudget; }
+    public List<Task> getTasks() { return List.copyOf(tasks); }
+
 }
